da92c1618acd86fcd39ae56ac540a93ae21379fd
R-739 <lchqq11003@yahoo.co.jp>
2020-08-27 00:33:08 +0000

    post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  
+  
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
+    else
+      @posts = current_user.posts.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:success] = 'メッセージを削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+
+  def edit
+   @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:success] = '投稿内容は正常に更新されました'
+      redirect_to root_url
+    else
+      flash.now[:danger] = '投稿内容は更新されませんでした。'
+      render :edit
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:content)
+  end
+  
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    unless @post
+      redirect_to root_url
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create, :destroy]
+  
+  resources :posts, only: [:create, :destroy, :edit, :update]
 end
